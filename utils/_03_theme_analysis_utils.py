3310f69127671708c3f6e698a1b333cea7e1864c
Ryan Perry <rperry2174@gmail.com>
2023-09-18 00:34:40 -0400

    Add better prompt for theme

--- utils/_03_theme_analysis_utils.py
+++ utils/_03_theme_analysis_utils.py
@@ -2,12 +2,29 @@ import openai
 from utils.initialize_open_ai import initialize_openai
 initialize_openai()
 
-def analyze_themes_with_chatgpt(unknown_words):
+def analyze_themes_with_chatgpt(unknown_words, learning_language="spanish", known_language="english" ):
     # Convert the list of unknown words into a string, separated by commas
     unknown_words_str = ', '.join(unknown_words)
     learning_language = "spanish"
     # Prepare the prompt for ChatGPT
-    prompt = f"Analyze the following list of unkown {learning_language} words and identify common themes from a linguistic perspective. This list is meant to eventually give some ideas for where one can focus their learning so that they can improve at {learning_language}: {unknown_words_str}."
+    prompt = f"""
+      I am a native {known_language} speaker who is learning {learning_language}.
+
+      I have identified the following {learning_language} words as being difficult for me to understand: {unknown_words_str} 
+
+      Analyze the that list of {learning_language} words and identify common themes
+      from a linguistic perspective. Some exmamples include, but are not limited to:
+      - common tenses
+      - common subject matter
+      - common parts of speech
+      - common prefixes or suffixes
+      - common roots
+      - common conjugations
+
+      This list is meant to eventually give some ideas for where I can focus my learning.
+      Write the themese in {known_language} in a clear and simple bulleted format with short descriptions. 
+      If no reasonable theme can be found then say "no theme found".
+    """
 
     # Interact with ChatGPT to get its analysis
     response = openai.Completion.create(
